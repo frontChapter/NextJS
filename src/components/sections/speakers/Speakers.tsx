5b61d1af0c8b05c427218a7bf5314127cf69db00
dofxo <kargat504@gmail.com>
2025-01-27 18:05:01 +0330

    feat: add speakers section's wrapper with its title
    in this commit i've added speaker section wrapper with its styles along
    with  the title of it.
    as mentioned in the code, hard coded sizes will be replaced with
    tailwind custom configs (color, spacings at etc.)

---
+++ src/components/sections/speakers/Speakers.tsx
@@ -0,0 +1,16 @@
+const Speakers = () => {
+  {
+    /* TODO: replace hard coded sizes with global props */
+  }
+
+  return (
+    <section
+      id="speakers"
+      className="flex flex-col gap-[48px] px-[32px] py-[80px] text-[#FAFAFA]"
+    >
+      <h3 className="text-center text-[48px] font-[800]">سخنرانان همایش</h3>
+    </section>
+  );
+};
+
+export default Speakers;
