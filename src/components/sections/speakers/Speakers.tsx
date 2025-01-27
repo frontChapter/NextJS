325f0394f6893855f036b8338cc47123d6a8568a
dofxo <kargat504@gmail.com>
2025-01-27 19:46:28 +0330

    modify: render speakers using speakers data

--- src/components/sections/speakers/Speakers.tsx
+++ src/components/sections/speakers/Speakers.tsx
@@ -1,3 +1,6 @@
+import Speaker from "./Speaker";
+import { speakersData } from "@/data/speakers";
+
 const Speakers = () => {
   {
     /* TODO: replace hard coded sizes with global props */
@@ -6,9 +9,14 @@ const Speakers = () => {
   return (
     <section
       id="speakers"
-      className="flex flex-col gap-[48px] px-[32px] py-[80px] text-[#FAFAFA]"
+      className="flex flex-col items-center gap-[48px] px-[32px] py-[80px] text-[#FAFAFA]"
     >
       <h3 className="text-center text-[48px] font-[800]">سخنرانان همایش</h3>
+      <div className="speakers-wrapper flex max-w-[1080px] flex-wrap justify-center gap-[20px]">
+        {speakersData.map((info, idx) => (
+          <Speaker key={idx} speakerInfo={info} />
+        ))}
+      </div>
     </section>
   );
 };
