11d0030b3a47d1c8579539713635fc737119862b
Majid Kargar <majidkargar01@gmail.com>
2025-01-27 18:50:08 +0330

    feat: Add theme provider and use it in main layout
    The themeProvider was used from the launch UI library template.
    
    I've also changed the lang of html to fa-IR and set the colorScheme to dark and className to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
+import "./globals.css";
+import { ThemeProvider } from "@/components/contexts/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,11 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="fa-IR" style={{colorScheme:"dark"}} className="dark" >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
