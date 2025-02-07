0de7e93a0fa134caf321415014de62204aba014c
oliver <mahdig502@gmail.com>
2025-02-07 13:15:33 +0330

    feat: add Hero component

---
+++ src/app/components/Hero/Hero.tsx
@@ -0,0 +1,12 @@
+import { Section } from "@/components/ui/section";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <Section>
+      <div>Hero</div>
+    </Section>
+  );
+};
+
+export default Hero;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Hero from "./components/Hero/Hero";
 import FAQ from "@/app/components/FAQ/FAQ";
 
 export default function Home() {
   return (
     <>
+      <Hero />
       <FAQ />
     </>
   );
