def50d03a6fa005f93530bcfde56caf10d3e991d
dofxo <kargat504@gmail.com>
2025-02-09 00:12:16 +0330

    feat: add speakers data

--- src/configs/speakers.ts
+++ src/configs/speakers.ts
@@ -1,86 +1,59 @@
 import { speakerInfoType } from "../app/components/speakers/Speakers.types";
 
 export const speakersData: speakerInfoType[] = [
-  // all data will be replaced/removed with actual data
   {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
+    name: "عرفان عباسی",
+    role: "مدیر فنی",
+    imageUrl: "/images/speakers/profile-picture/erfan-abbasi.jpeg",
+    company: {
+      name: " کارگزاری مفید",
+      logo: "/images/speakers/company-logo/mofid.png",
+    },
+  },
+  {
+    name: "مجتبی افراز",
+    role: "مهندس نرم‌افزار",
+    imageUrl: "/images/speakers/profile-picture/mojtaba-afraz.jpeg",
+    company: {
+      name: "تپسل",
+      logo: "/images/speakers/company-logo/tapsell.png",
+    },
+  },
+  {
+    name: "امیرحسین دارابی",
+    role: "منتور و مدرس",
+    imageUrl: "/images/speakers/profile-picture/amirhossein-darabi.jpeg",
+    company: {
+      name: "کوئرا",
+      logo: "/images/speakers/company-logo/quera.png",
+    },
+  },
+  {
+    name: "محمد باقر عبیات",
+    role: "بنیانگذار",
+    imageUrl: "/images/speakers/profile-picture/mohammad-baqer-abiat.jpeg",
+    company: {
+      name: "TinyLibs",
+      logo: "/images/speakers/company-logo/tiny-libs.png",
+    },
+  },
+  {
+    name: "مسعود بیگی",
+    role: "CTO",
+    imageUrl: "/images/speakers/profile-picture/masoud-beygi.jpeg",
     company: {
       name: "رسمیو",
       logo: "/images/speakers/company-logo/rasmio.png",
     },
   },
+
   {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
+    name: "سجاد منشی",
+    role: "مهندس نرم‌افزار",
+    imageUrl: "/images/speakers/profile-picture/sajjad-maneshi.jpeg",
     company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
-    },
-  },
-  {
-    name: "پوریا باباعلی",
-    role: "توسعده دهنده فرانت‌اند",
-    imageUrl: "/images/speakers/profile-picture/pooria-baba-ali.webp",
-    company: {
-      name: "رسمیو",
-      logo: "/images/speakers/company-logo/rasmio.png",
+      name: "Bazaro",
+      logo: "/images/speakers/company-logo/bazaro.png",
     },
   },
 ];
