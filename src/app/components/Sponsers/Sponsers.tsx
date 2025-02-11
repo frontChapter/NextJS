71f6e51bf270ad85a6c916f73f5e5825e52d6580
bravemobin <mobinkhan.ashatejarat@gmail.com>
2025-02-11 19:48:54 +0330

    feat: Add sponsors section to the website

--- src/app/components/Sponsers/Sponsers.tsx
+++ src/app/components/Sponsers/Sponsers.tsx
@@ -1,20 +1,31 @@
 import { Section } from "@/components/ui/section";
+import sponsersData from "@/configs/sponsersData";
+import Image from "next/image";
 
 export default function Sponsers() {
   return (
     <Section>
-      <div className="mx-auto flex max-w-container flex-col items-center gap-8 text-center">
+      <div className="mx-auto flex max-w-container flex-col items-center gap-14 text-center">
         <div className="flex flex-col items-center gap-6">
-          <h2 className="text-md font-semibold sm:text-2xl">
-            Built with industry-standard tools and best practices
+          <h2 className="text-md font-semibold sm:text-xl">
+            حامیان همایش فرانت چپتر
           </h2>
         </div>
         <div className="flex flex-wrap items-center justify-center gap-8">
-          {/* <Logo image={Figma} name="Figma" />
-          <Logo image={React} name="React" version="19.0.0" />
-          <Logo image={TypeScript} name="TypeScript" version="5.6.3" />
-          <Logo image={ShadcnUi} name="Shadcn/ui" version="2.1.8" />
-          <Logo image={Tailwind} name="Tailwind" version="3.4.14" /> */}
+          {sponsersData.map((sponser, index) => (
+            <div className="flex items-center gap-2" key={index}>
+              <div className="border-2 border-gray-800 p-1">
+                <Image
+                  src={sponser.image}
+                  width={21}
+                  height={21}
+                  alt={sponser.title}
+                />
+              </div>
+
+              <span className="font-bold">{sponser.title}</span>
+            </div>
+          ))}
         </div>
       </div>
     </Section>
