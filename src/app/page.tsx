1de6086a07c287dcbb284016edbd07568ceb5d0f
bravemobin <mobinkhan.ashatejarat@gmail.com>
2025-02-11 19:16:51 +0330

    feat: Add Sponsors section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Sponsers from "./components/Sponsers/Sponsers";
 import FAQ from "@/app/components/FAQ/FAQ";
 
 export default function Home() {
   return (
     <>
       <FAQ />
+      <Sponsers />
     </>
   );
 }
