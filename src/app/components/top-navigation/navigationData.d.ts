4c29d515f55d9ed80810b8af8633645b62370722
realamirshad <amirshadnew@gmail.com>
2025-02-12 03:41:46 +0330

    feat: navigation data configuration

---
+++ src/app/components/top-navigation/navigationData.d.ts
@@ -0,0 +1,1 @@
+export type NavigationDataType = { content: string; link: string };
---
+++ src/configs/NavigationData.ts
@@ -0,0 +1,16 @@
+import { NavigationDataType } from "@/app/components/top-navigation/navigationData";
+
+export const NavigationData: NavigationDataType[] = [
+  {
+    content: "شروع",
+    link: "/",
+  },
+  {
+    content: "سخنرانان",
+    link: "/",
+  },
+  {
+    content: "خرید بلیط",
+    link: "/",
+  },
+];
