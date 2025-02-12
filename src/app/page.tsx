33c2da5bbfb47391fdc0c302748371c1e4622233
realamirshad <amirshadnew@gmail.com>
2025-02-12 03:44:02 +0330

    feat: update page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import TopNavigation from "./components/top-navigation/top-navigation";
 import FAQ from "@/app/components/FAQ/FAQ";
 
 export default function Home() {
   return (
     <>
+      <TopNavigation />
       <FAQ />
     </>
   );
