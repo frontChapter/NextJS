a4f47828e45d27b0f9afe2f7047467d312616bc2
oliver <mahdig502@gmail.com>
2025-02-15 18:24:59 +0330

    fix: make half circle(HeroSVG) full width for large devices

--- src/app/components/Hero/Hero.tsx
+++ src/app/components/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="relative min-h-screen w-full overflow-hidden bg-background px-4 pb-[500px] text-foreground dark:bg-background-dark dark:text-foreground-dark md:px-8">
+    <div className="relative w-full overflow-hidden bg-background px-4 pb-[400px] text-foreground dark:bg-background-dark dark:text-foreground-dark md:px-8">
       <HeroContent />
       <div className="absolute -left-[26%] -top-[150px] z-10 hidden items-center justify-center xl:flex min-[1440px]:-left-[22%] 2xl:-left-[18%]">
         <HeroLeftTech />
@@ -13,7 +13,7 @@ const Hero = () => {
       <div className="absolute -right-[30%] -top-[150px] z-10 hidden items-center justify-center xl:flex min-[1440px]:-right-[25%] 2xl:-right-[21.5%]">
         <HeroRightTech />
       </div>
-      <div className="absolute left-1/2 top-[345px] -translate-x-1/2">
+      <div className="absolute left-1/2 top-[345px] -translate-x-1/2 min-[1650px]:w-full min-[1650px]:max-w-[100%]">
         <HeroSVG />
       </div>
     </div>
